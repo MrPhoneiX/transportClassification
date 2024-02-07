d53ce1785ef235d770f3a59804347b1856d474ba
MrPhoneiX <100272193+MrPhoneiX@users.noreply.github.com>
2024-02-07 22:53:49 +0500

    commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=['png','jpeg','gif','svg'])
+if file:
+    st.image(file)
+
+    # PIL convert
+    img = PILImage.create(file)
+
+    # model
+    fname = '/Users/dartikov/Documents/Projects/DataScient_AI/transports/transport_model.pkl'
+    model = load_learner(fname)
+
+    #predict
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(f'Bashorat: {pred}')
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.2f}%')
+    
+    #plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
+
