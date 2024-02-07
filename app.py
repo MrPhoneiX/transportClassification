193310fc14054174e691f5570b553b132f81bccb
Dilshod Artikov <100272193+MrPhoneiX@users.noreply.github.com>
2024-02-07 23:29:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from fastai.vision.all import *
 import plotly.express as px
+import pathlib
+import platform
+
+plt = platform.system()
+if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 
 st.title('Transportni klassifikatsiya qiluvchi model')
